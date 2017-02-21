cdd161aab3080ea286b286195050dca7ad7d2415
Vishvaa <vzt0019@auburn.edu>
2017-02-20 23:22:37 -0600

    Red Light |

--- softwareprocess/Sample.py
+++ softwareprocess/Sample.py
@@ -64,11 +64,9 @@ class Sample(object):
         result = base ** exponent
         return result
     
-    def integrate(self, t, n, f):
+    def integrate(self, lowBound, highBound, n, f):
         epsilon = 0.001
         simpsonOld = 0.0
-        lowBound = 0
-        highBound = t
         simpsonNew = epsilon
         s = 4
         while abs((simpsonNew-simpsonOld)/simpsonNew) > epsilon:
@@ -77,13 +75,13 @@ class Sample(object):
             w1 = w / 3
             intf = f(lowBound, n)
             while (lowBound + w) < highBound:
-                exp1 = 4(self.f(lowBound + self.w, n))
+                exp1 = 4(f(lowBound + w, n))
                 w += w
                 if (lowBound + w) < highBound:
-                    exp2 = 2(self.f(lowBound + self.w, n))
+                    exp2 = 2(f(lowBound + w, n))
                     w += w
                 total = total + exp1 + exp2
-            simpsonNew = w1 * (intf + total + self.f(highBound, n))
+            simpsonNew = w1 * (intf + total + f(highBound, n))
             s *= 2
         return simpsonNew
 
