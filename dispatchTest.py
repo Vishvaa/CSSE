d16486cff61a960cd68a6a37e57d049653056823
Vishvaa <vzt0019@auburn.edu>
2017-03-22 01:45:58 -0500

    Red Light |

--- dispatchTest.py
+++ dispatchTest.py
@@ -9,24 +9,24 @@ class dispatch(TestCase):
 
 # Happy Path tests
 
-    # Should calculate altitude on all the values provided
+    # Should calculate altitude on all the values provided      Done
     # Should calculate altitude on missing height
     # Should calculate altitude on missing pressure
     # Should calculate altitude on missing temperature
     # Should calculate altitude on missing horizon
     # Should calculate altitude from observation
 
-    def test_100_010_ShouldcalculateAltitudeFromObservation(self):
-        pass
-
-
-    def test_200_020_ShouldCalculateAltitudeWithMandatoryInformation(self):
+    def test_100_010_ShouldCalculateAltitudeWithMandatoryInformation(self):
         param = {'observation': '45d15.2', 'height': '6.0', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71'}
         expectedparam = {'altitude': '45d11.9', 'observation': '45d15.2', 'height': '6.0', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71'}
         print dspt.dispatch(param)
         self.assertDictEqual(dspt.dispatch(param), expectedparam, "Not Able to pass Dict with All Mandatory Information Provided.")
 
-
+    def test_100_010_ShouldcalculateAltitudeFromObservation(self):
+        param = {'observation': '45d15.2', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71'}
+        expectedparam = {'altitude': '45d11.9', 'observation': '45d15.2', 'height': '6.0', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71'}
+        print dspt.dispatch(param)
+        self.assertDictEqual(dspt.dispatch(param), expectedparam, "Not Able to pass Dict with All Mandatory Information Provided.")
 
 
 
