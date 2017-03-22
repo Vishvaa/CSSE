55a6fcbd474919e71b4427ee3e650c4b0db93760
Vishvaa <vzt0019@auburn.edu>
2017-03-22 02:10:56 -0500

    Red Light |

--- dispatchTest.py
+++ dispatchTest.py
@@ -10,23 +10,28 @@ class dispatch(TestCase):
 # Happy Path tests
 
     # Should calculate altitude on all the values provided      Done
-    # Should calculate altitude on missing height
+    # Should calculate altitude on missing height               Done
     # Should calculate altitude on missing pressure
     # Should calculate altitude on missing temperature
     # Should calculate altitude on missing horizon
     # Should calculate altitude from observation
-    # Should pass with extra info provided
+    # Should pass with extra info provided                      Done
 
     def test_100_010_ShouldCalculateAltitudeWithMandatoryInformation(self):
-        param = {'observation': '45d15.2', 'height': '6.0', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71','extra':'extra'}
-        expectedparam = {'altitude': '45d11.9', 'observation': '45d15.2', 'height': '6.0', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71','extra':'extra'}
+        param = {'observation': '45d15.2', 'height': '6.0', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71',}
+        expectedparam = {'altitude': '45d11.9', 'observation': '45d15.2', 'height': '6.0', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71'}
         self.assertDictEqual(dspt.dispatch(param), expectedparam, "Not Able to pass Dict with All Mandatory Information Provided.")
 
-    def test_100_010_ShouldcalculateAltitudeFromObservation(self):
+    def test_100_020_ShouldcalculateAltitudewithoutHeight(self):
         param = {'observation': '45d15.2', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71'}
         expectedparam = {'altitude': '45d14.3', 'observation': '45d15.2', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71'}
         print dspt.dispatch(param)
         self.assertDictEqual(dspt.dispatch(param), expectedparam, "Not Able to pass Dict with Missing Height.")
+
+    def test_100_030_ShouldCalculateAltitudeWithMandatoryInformation(self):
+        param = {'observation': '45d15.2', 'height': '6.0', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71','extra':'extra'}
+        expectedparam = {'altitude': '45d11.9', 'observation': '45d15.2', 'height': '6.0', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71','extra':'extra'}
+        self.assertDictEqual(dspt.dispatch(param), expectedparam, "Not Able to pass Dict with Extra Information Provided.")
 
 
 
