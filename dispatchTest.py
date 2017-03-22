601d771eaea2faa91e0f8b67a18a608383535431
Vishvaa <vzt0019@auburn.edu>
2017-03-22 02:09:35 -0500

    Green Light |

--- dispatchTest.py
+++ dispatchTest.py
@@ -15,6 +15,7 @@ class dispatch(TestCase):
     # Should calculate altitude on missing temperature
     # Should calculate altitude on missing horizon
     # Should calculate altitude from observation
+    # Should pass with extra info provided
 
     def test_100_010_ShouldCalculateAltitudeWithMandatoryInformation(self):
         param = {'observation': '45d15.2', 'height': '6.0', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71','extra':'extra'}
@@ -23,7 +24,7 @@ class dispatch(TestCase):
 
     def test_100_010_ShouldcalculateAltitudeFromObservation(self):
         param = {'observation': '45d15.2', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71'}
-        expectedparam = {'altitude': '45d11.9', 'observation': '45d15.2', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71'}
+        expectedparam = {'altitude': '45d14.3', 'observation': '45d15.2', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71'}
         print dspt.dispatch(param)
         self.assertDictEqual(dspt.dispatch(param), expectedparam, "Not Able to pass Dict with Missing Height.")
 
