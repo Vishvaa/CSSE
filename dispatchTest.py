0e6be49ce6fd73f3ec68c2d8eb34a15b171ac7a8
Vishvaa <vzt0019@auburn.edu>
2017-03-22 03:03:47 -0500

    Performing Tests

--- dispatchTest.py
+++ dispatchTest.py
@@ -21,59 +21,59 @@ class dispatch(TestCase):
 
     # Should calculate Altitude with missing value of height        Done
     # Should calculate Altitude with missing value of pressure      Done
-    # Should calculate Altitude with missing value of horizon
+    # Should calculate Altitude with missing value of horizon       Done
     # Should calculate Altitude with missing value of temperature   Done
 
-    # def test_100_010_ShouldCalculateAltitudeWithMandatoryInformation(self):
-    #     param = {'observation': '45d15.2', 'height': '6.0', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71',}
-    #     expectedparam = {'altitude': '45d11.9', 'observation': '45d15.2', 'height': '6.0', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71'}
-    #     self.assertDictEqual(dspt.dispatch(param), expectedparam, "Not Able to pass Dict with All Mandatory Information Provided.")
-    #
-    # def test_100_020_ShouldcalculateAltitudewithoutHeight(self):
-    #     param = {'observation': '45d15.2', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71'}
-    #     expectedparam = {'altitude': '45d14.3', 'observation': '45d15.2', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71'}
-    #     self.assertDictEqual(dspt.dispatch(param), expectedparam, "Not Able to pass Dict with Missing Height.")
-    #
-    # def test_100_030_ShouldCalculateAltitudeWithMandatoryInformation(self):
-    #     param = {'observation': '45d15.2', 'height': '6.0', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71','extra':'extra'}
-    #     expectedparam = {'altitude': '45d11.9', 'observation': '45d15.2', 'height': '6.0', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71','extra':'extra'}
-    #     self.assertDictEqual(dspt.dispatch(param), expectedparam, "Not Able to pass Dict with Extra Information Provided.")
-    #
-    # def test_100_040_ShouldcalculateAltitudewithoutPressure(self):
-    #     param = {'observation': '45d15.2', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71'}
-    #     expectedparam = {'altitude': '45d14.3', 'observation': '45d15.2', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71'}
-    #     self.assertDictEqual(dspt.dispatch(param), expectedparam, "Not Able to pass Dict with Missing Pressure.")
-    #
-    # def test_100_050_ShouldcalculateAltitudewithoutTemperature(self):
-    #     param = {'observation': '45d15.2', 'horizon': 'natural', 'op': 'adjust'}
-    #     expectedparam = {'altitude': '45d14.3', 'observation': '45d15.2', 'horizon': 'natural', 'op': 'adjust'}
-    #     self.assertDictEqual(dspt.dispatch(param), expectedparam, "Not Able to pass Dict with Missing Temperature.")
-    #
-    # def test_100_060_ShouldcalculateAltitudewithoutHorizon(self):
-    #     param = {'observation': '42d0.0', 'op': 'adjust'}
-    #     expectedparam = {'altitude': '41d59.0', 'observation': '42d0.0', 'op': 'adjust'}
-    #     self.assertDictEqual(dspt.dispatch(param), expectedparam, "Not Able to pass Dict with Missing Horizon.")
-    #
-    #
-    # def test_100_070_ShouldcalculateAltitudewithoutAllOptionalValues(self):
-    #     param = {'observation': '45d15.2', 'op': 'adjust'}
-    #     expectedparam = {'altitude': '45d14.3', 'observation': '45d15.2', 'op': 'adjust'}
-    #     self.assertDictEqual(dspt.dispatch(param), expectedparam, "Not Able to pass Dict with Missing All Optional Values.")
-    #
-    # def test_200_010_ShouldcalculateAltitudewithBlankHeight(self):
-    #     param = {'observation': '45d15.2', 'op': 'adjust', 'height':''}
-    #     expectedparam = {'altitude': '45d14.3', 'observation': '45d15.2', 'op': 'adjust','height':''}
-    #     self.assertDictEqual(dspt.dispatch(param), expectedparam, "Not Able to pass Dict with Missing Value of height.")
-    #
-    # def test_200_020_ShouldcalculateAltitudewithBlankPressure(self):
-    #     param = {'observation': '45d15.2', 'op': 'adjust', 'height':'', 'pressure':''}
-    #     expectedparam = {'altitude': '45d14.3', 'observation': '45d15.2', 'op': 'adjust','height':'', 'pressure':''}
-    #     self.assertDictEqual(dspt.dispatch(param), expectedparam, "Not Able to pass Dict with Missing Value of pressure.")
-    #
-    # def test_200_030_ShouldcalculateAltitudewithBlankTemperature(self):
-    #     param = {'observation': '45d15.2', 'op': 'adjust', 'height':'', 'pressure':'','temperature':''}
-    #     expectedparam = {'altitude': '45d14.3', 'observation': '45d15.2', 'op': 'adjust','height':'', 'pressure':'','temperature':''}
-    #     self.assertDictEqual(dspt.dispatch(param), expectedparam, "Not Able to pass Dict with Missing Value of temperature.")
+    def test_100_010_ShouldCalculateAltitudeWithMandatoryInformation(self):
+        param = {'observation': '45d15.2', 'height': '6.0', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71',}
+        expectedparam = {'altitude': '45d11.9', 'observation': '45d15.2', 'height': '6.0', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71'}
+        self.assertDictEqual(dspt.dispatch(param), expectedparam, "Not Able to pass Dict with All Mandatory Information Provided.")
+
+    def test_100_020_ShouldcalculateAltitudewithoutHeight(self):
+        param = {'observation': '45d15.2', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71'}
+        expectedparam = {'altitude': '45d14.3', 'observation': '45d15.2', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71'}
+        self.assertDictEqual(dspt.dispatch(param), expectedparam, "Not Able to pass Dict with Missing Height.")
+
+    def test_100_030_ShouldCalculateAltitudeWithMandatoryInformation(self):
+        param = {'observation': '45d15.2', 'height': '6.0', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71','extra':'extra'}
+        expectedparam = {'altitude': '45d11.9', 'observation': '45d15.2', 'height': '6.0', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71','extra':'extra'}
+        self.assertDictEqual(dspt.dispatch(param), expectedparam, "Not Able to pass Dict with Extra Information Provided.")
+
+    def test_100_040_ShouldcalculateAltitudewithoutPressure(self):
+        param = {'observation': '45d15.2', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71'}
+        expectedparam = {'altitude': '45d14.3', 'observation': '45d15.2', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71'}
+        self.assertDictEqual(dspt.dispatch(param), expectedparam, "Not Able to pass Dict with Missing Pressure.")
+
+    def test_100_050_ShouldcalculateAltitudewithoutTemperature(self):
+        param = {'observation': '45d15.2', 'horizon': 'natural', 'op': 'adjust'}
+        expectedparam = {'altitude': '45d14.3', 'observation': '45d15.2', 'horizon': 'natural', 'op': 'adjust'}
+        self.assertDictEqual(dspt.dispatch(param), expectedparam, "Not Able to pass Dict with Missing Temperature.")
+
+    def test_100_060_ShouldcalculateAltitudewithoutHorizon(self):
+        param = {'observation': '42d0.0', 'op': 'adjust'}
+        expectedparam = {'altitude': '41d59.0', 'observation': '42d0.0', 'op': 'adjust'}
+        self.assertDictEqual(dspt.dispatch(param), expectedparam, "Not Able to pass Dict with Missing Horizon.")
+
+
+    def test_100_070_ShouldcalculateAltitudewithoutAllOptionalValues(self):
+        param = {'observation': '45d15.2', 'op': 'adjust'}
+        expectedparam = {'altitude': '45d14.3', 'observation': '45d15.2', 'op': 'adjust'}
+        self.assertDictEqual(dspt.dispatch(param), expectedparam, "Not Able to pass Dict with Missing All Optional Values.")
+
+    def test_200_010_ShouldcalculateAltitudewithBlankHeight(self):
+        param = {'observation': '45d15.2', 'op': 'adjust', 'height':''}
+        expectedparam = {'altitude': '45d14.3', 'observation': '45d15.2', 'op': 'adjust','height':''}
+        self.assertDictEqual(dspt.dispatch(param), expectedparam, "Not Able to pass Dict with Missing Value of height.")
+
+    def test_200_020_ShouldcalculateAltitudewithBlankPressure(self):
+        param = {'observation': '45d15.2', 'op': 'adjust', 'height':'', 'pressure':''}
+        expectedparam = {'altitude': '45d14.3', 'observation': '45d15.2', 'op': 'adjust','height':'', 'pressure':''}
+        self.assertDictEqual(dspt.dispatch(param), expectedparam, "Not Able to pass Dict with Missing Value of pressure.")
+
+    def test_200_030_ShouldcalculateAltitudewithBlankTemperature(self):
+        param = {'observation': '45d15.2', 'op': 'adjust', 'height':'', 'pressure':'','temperature':''}
+        expectedparam = {'altitude': '45d14.3', 'observation': '45d15.2', 'op': 'adjust','height':'', 'pressure':'','temperature':''}
+        self.assertDictEqual(dspt.dispatch(param), expectedparam, "Not Able to pass Dict with Missing Value of temperature.")
 
     def test_200_040_ShouldcalculateAltitudewithBlankHorizon(self):
         param = {'observation': '10d0', 'op': 'adjust','horizon':'artifical', 'height':'6.0', 'pressure':'1010','temperature':'72'}
@@ -89,7 +89,9 @@ class dispatch(TestCase):
     # Should generate error on wrong temperature input
     # Should generate error on missing mandatory information
 
-
-
+    def test_300_010_ShouldGenerateErrorOnWrongObservatio(self):
+        param = {'observation': '10d0', 'op': 'adjust','horizon':'artifical', 'height':'6.0', 'pressure':'1010','temperature':'72'}
+        expectedparam = {'altitude': '9d54.7', 'observation': '10d0', 'op': 'adjust','horizon':'artifical','height':'6.0', 'pressure':'1010','temperature':'72'}
+        self.assertDictEqual(dspt.dispatch(param), expectedparam, "Not Able to pass Dict with Missing Value of horizon.")
 
 
