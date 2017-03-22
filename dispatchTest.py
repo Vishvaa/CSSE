1a828b1ea9018c32e7d95835ccb91b43fd790d43
Vishvaa <vzt0019@auburn.edu>
2017-03-22 16:15:19 -0500

    Green Light |

--- dispatchTest.py
+++ dispatchTest.py
@@ -99,7 +99,7 @@ class dispatch(TestCase):
         expectedparam = {'observation': '-10d0', 'op': 'adjust','horizon':'artifical','height':'6.0', 'pressure':'1010','temperature':'72','error': 'observation is invalid'}
         self.assertDictEqual(dspt.dispatch(param), expectedparam, "Not Able to pass Dict with Invalid value of Observation Degree.")
 
-    def test_300_010_ShouldGenerateErrorOnWrongObservationDegree(self):
+    def test_300_030_ShouldGenerateErrorOnWrongObservationDegree(self):
         param = {'observation': '10d60.1', 'op': 'adjust','horizon':'artifical', 'height':'6.0', 'pressure':'1010','temperature':'72'}
-        expectedparam = {'observation': '100d60.1', 'op': 'adjust','horizon':'artifical','height':'6.0', 'pressure':'1010','temperature':'72','error': 'observation is invalid'}
+        expectedparam = {'observation': '10d60.1', 'op': 'adjust','horizon':'artifical','height':'6.0', 'pressure':'1010','temperature':'72','error': 'observation is invalid'}
         self.assertDictEqual(dspt.dispatch(param), expectedparam, "Not Able to pass Dict with Invalid value of Observation Degree.")
