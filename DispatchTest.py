111ab2355809c0e4949a997249d9f809460cedef
Vishvaa <vzt0019@auburn.edu>
2017-04-07 19:25:19 -0700

    Green Light |

--- DispatchTest.py
+++ DispatchTest.py
@@ -333,5 +333,4 @@ class MyTestCase(unittest.TestCase):
 
     def test400_050ShouldReturnErrorWhenWrongTimeFormat(self):
         values = {'op':'predict','body':'betelgeuse','date':'2016-1-17','time':'03:65:42'}
-        print (DP.dispatch(values))
         self.assertTrue(DP.dispatch(values).has_key("error"), True)
