08730c43a1da2e3e470bdb9fffd37aec8c472fe5
Vishvaa <vzt0019@auburn.edu>
2017-04-27 01:37:08 -0700

    Green Light |

--- DispatchTest.py
+++ DispatchTest.py
@@ -423,9 +423,10 @@ class MyTestCase(unittest.TestCase):
 
     def test400_010ShouldcalculateCorrectedDistanceandAzimuthwithProperData(self):
         values = {'op':'correct', 'lat':'16d32.3', 'long':'95d41.6', 'altitude':'13d42.3',  'assumedLat':'-53d38.4', 'assumedLong':' 74d35.3'}
-        expectedValues = {'op':'correct', 'lat':'16d32.3', 'long':'95d41.6', 'altitude':'13d42.3',  'assumedLat':'-53d38.4', 'assumedLong':' 74d35.3', 'correctedDistance':'3950', 'correctedAzimuth':'164d42.9'}
         print (DP.dispatch(values))
-        self.assertDictEqual(values, expectedValues, "error: Values do not match")
+        expectedValues = {'op':'correct', 'lat':'16d32.3', 'long':'95d41.6', 'altitude':'13d42.3',  'assumedLat':'-53d38.4', 'assumedLong':' 74d35.3', 'correctedDistance':'3950', 'correctedAzimuth':'164d42.9'}
+        self.assertDictEqual(values, expectedValues)
+
 
     def test400_020ShouldReturnErrorONMissingLat(self):
         values = {'op':'correct', 'long':'95d41.6', 'altitude':'13d42.3',  'assumedLat':'-53d38.4', 'assumedLong':' 74d35.3'}
